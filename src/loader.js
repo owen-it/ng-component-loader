8aeeeee132d44d137af2f6e5104179782e9a8886
Antério Vieira <anteriovieira@gmail.com>
2017-01-24 18:31:18 -0200

    Updated

--- src/loader.js
+++ src/loader.js
@@ -1,3 +1,35 @@
-module.exports = function(source){
-  return source;
-}
+var consolidate = require('consolidate');
+var utils = require('loader-utils');
+var extname = require('path').extname;
+
+module.exports = function(content) {
+  this.cacheable && this.cacheable();
+
+  var callback = this.async();
+  var opt = utils.parseQuery(this.query);
+
+  function exportContent(content) {
+    if (opt.raw) {
+      callback(null, content);
+    } else {
+      callback(null, "module.exports = " + JSON.stringify(content));
+    }
+  }
+
+  if(!opt.engine) {
+    opt.engine = extname(this.request).substr(1).toLowerCase();
+  }
+
+  if(!consolidate[opt.engine]) {
+    throw new Error("Engine '"+ opt.engine +"' isn't available in Consolidate.js");
+  }
+
+  opt.filename = this.resourcePath;
+
+  consolidate[opt.engine].render(content, opt, function(err, html) {
+    if(err) {
+      throw err;
+    }
+    exportContent(html);
+  });
+};
